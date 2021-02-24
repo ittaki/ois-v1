3c0bc198a00242e57695222e7991e6df0664fa6e
gregorkovac <gk1854@student.uni-lj.si>
2021-02-24 15:48:48 +0100

    Dodajanje izpisa

--- v1/Gravitacija.java
+++ v1/Gravitacija.java
@@ -1,6 +1,11 @@
 import java.util.*;
 
 public class Gravitacija {
+    
+    public void izpis(double rezultat) {
+        System.out.println(rezultat);
+    }
+
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
     }
