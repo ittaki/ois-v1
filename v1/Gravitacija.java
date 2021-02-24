7ea2377f7a0d18fa4bce6e5235df7742b8939535
Andjela <andjelarajovic01@gmail.com>
2021-02-24 17:08:27 +0100

    Popravljeno

--- v1/Gravitacija.java
+++ v1/Gravitacija.java
@@ -3,27 +3,28 @@ import java.util.*;
 public class Gravitacija {
 
 
-    public void izpis(double visina, double pospesek) {
+    public static void izpis(double visina, double pospesek) {
         System.out.println(visina + " " + pospesek);
     }
-
-    public static void main(String[] args) {
 
 	public static final double C = 6.674 * 0.00000000001;
 	public static final double M = 5.972 * 1000000000000000000000000.0;
 	public static final double r = 6.371 * 1000000;
 	
 	public static void main(String[] args) {
-		 
-		
-		
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
 
-        System.out.println("OIS je zakon!");
+		izpis(a,formula(a));
+
+
+
+        //System.out.println("OIS je zakon!");
 		
 	}
-    public static double formula(double nadmorska) {
+	public static double formula(double nadmorska) {
 		double a = (C * M) / ((r + nadmorska) * (r + nadmorska));
 		return a;
 	}
-	
 }
+
